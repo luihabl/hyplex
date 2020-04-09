b51a5954a0717aac814a8a456bed3bef063ce603
Lui Txai Calvoso Habl <txaicalvoso@gmail.com>
2020-04-09 11:39:28 -0300

    Changing slurm script to test

--- util/submit.sh
+++ util/submit.sh
@@ -9,6 +9,8 @@
 #SBATCH --output="%j.out"                               # Direct STDOUT here (file identifier), %j is substituted for the job number
 #SBATCH --verbose                                       # Increase informational messages
 #SBATCH --ntasks=1	                                    # Number of core (max 64) for your parallel job
+#SBATCH --time=10:00                                    # Maximum time 
+#SBATCH --test-only
 
 # Put your email here to receive execution location by mail
 echo "" |mail -s "Your job $SLURM_JOB_ID is running on $HOSTNAME" lui.habl@lpp.polytechnique.fr
