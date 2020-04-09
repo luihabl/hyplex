8901f8ed4d8b18028200da4d5b4beb031a096b60
Lui Txai Calvoso Habl <txaicalvoso@gmail.com>
2020-04-09 11:11:45 -0300

    Organizing slurm script

--- util/submit.sh
+++ util/submit.sh
@@ -1,41 +1,28 @@
 #!/bin/bash
-# First we set up the SLURM environment.
 
-#SBATCH --comment="Testing plume execution"          # An arbitrary comment on the job
-#SBATCH --job-name="pic plume"       # Give this job an arbitrary name
-#SBATCH --mail-type=ALL                          # When to send mail  (BEGIN, END, FAIL, REQUEUE, ALL)
-
-#SBATCH --mail-user=lui.habl@lpp.polytechnique.fr             # Where to send mail.  
-
-#SBATCH --workdir="/data/habl"   # Set current directory before job starts - LOGIN is your login
-
-#SBATCH --error="%j.err"            # Direct STDERR here (file identifier),
-                                                        # %j is substituted for the job number
-
-#SBATCH --output="%j.out"         # Direct STDOUT here (file identifier),
-                                                        # %j is substituted for the job number
-
-#SBATCH --requeue                      # On failure, requeue for another try
-
-#SBATCH --verbose                      # Increase informational messages
-
-#SBATCH --ntasks=1	        # Number of core (max 64) for your parallel job
+#SBATCH --comment="Testing Hyplex execution"            # An arbitrary comment on the job
+#SBATCH --job-name="pic plume"                          # Give this job an arbitrary name
+#SBATCH --mail-type=ALL                                 # When to send mail  (BEGIN, END, FAIL, REQUEUE, ALL)
+#SBATCH --mail-user=lui.habl@lpp.polytechnique.fr       # Where to send mail.  
+#SBATCH --workdir="/home/LPP/lui.habl/hyplex"           # Set current directory before job starts - LOGIN is your login
+#SBATCH --error="%j.err"                                # Direct STDERR here (file identifier), %j is substituted for the job number
+#SBATCH --output="%j.out"                               # Direct STDOUT here (file identifier), %j is substituted for the job number
+#SBATCH --verbose                                       # Increase informational messages
+#SBATCH --ntasks=1	                                    # Number of core (max 64) for your parallel job
 
 # Put your email here to receive execution location by mail
-
 echo "" |mail -s "Your job $SLURM_JOB_ID is running on $HOSTNAME" lui.habl@lpp.polytechnique.fr
 
 # Here you have to charge your module
 
-module load openmpi/4.0.2
-module load hdf5-openmpi-4.0.2/1.10.5
-module load hypre/2.11.1 
+module load gcc/7.1.0
+module load make/4.2
+module load openmpi/gcc/64/3.1.4
+module load hypre/2.14.0
 
 # Here the code to execute with the SRUN command
-# THIS IS AN EXEMPLE
 
-srun /home/habl/pic-plume/run /home/habl/pic-plume/input/config/config-zoid.ini
-
+srun ./run #/home/habl/pic-plume/input/config/config-zoid.ini
 
 # Make sure you return zero as exit code otherwiser SLURM will report your job
 # as failed.
