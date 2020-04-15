0e34e266c11b6b7d4f01a844e0771de49e40b5fc
Lui Txai Calvoso Habl <txaicalvoso@gmail.com>
2020-04-15 15:15:52 -0300

    Configuring for full run

--- util/submit.sh
+++ util/submit.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-#SBATCH --comment="Testing Hyplex execution"            # An arbitrary comment on the job
-#SBATCH --job-name="ht01"                               # Give this job an arbitrary name
+#SBATCH --comment="Testing Hyplex complete run"         # An arbitrary comment on the job
+#SBATCH --job-name="hf01"                               # Give this job an arbitrary name
 #SBATCH --mail-type=ALL                                 # When to send mail  (BEGIN, END, FAIL, REQUEUE, ALL)
 #SBATCH --mail-user=lui.habl@lpp.polytechnique.fr       # Where to send mail.  
 #SBATCH --workdir="/home/LPP/lui.habl/hyplex"           # Set current directory before job starts - LOGIN is your login
-#SBATCH --error="output/%j.err"                         # Direct STDERR here (file identifier), %j is substituted for the job number
-#SBATCH --output="output/%j.out"                        # Direct STDOUT here (file identifier), %j is substituted for the job number
+#SBATCH --error="output/log/%j.err"                         # Direct STDERR here (file identifier), %j is substituted for the job number
+#SBATCH --output="output/log/%j.out"                        # Direct STDOUT here (file identifier), %j is substituted for the job number
 #SBATCH --verbose                                       # Increase informational messages
-#SBATCH --ntasks=3                                      # Number of core (max 64) for your parallel job
+#SBATCH --ntasks=1                                      # Number of core (max 64) for your parallel job
 #SBATCH --cpus-per-task=3                               # Number of CPUs per task
-#SBATCH --time=03:00                                    # Maximum time 
 
 # Put your email here to receive execution location by mail
 echo "" |mail -s "Your job $SLURM_JOB_ID is running on $HOSTNAME" lui.habl@lpp.polytechnique.fr
