01469a66757a10c3fc1562c2b462d280b27dd9ac
Lui Txai Calvoso Habl <txaicalvoso@gmail.com>
2020-04-22 19:14:11 -0300

    Changing submission script

--- util/submit.sh
+++ util/submit.sh
@@ -8,20 +8,47 @@
 #SBATCH --error="output/log/%j.err"                         # Direct STDERR here (file identifier), %j is substituted for the job number
 #SBATCH --output="output/log/%j.out"                        # Direct STDOUT here (file identifier), %j is substituted for the job number
 #SBATCH --verbose                                       # Increase informational messages
+
 #SBATCH --ntasks=1                                      # Number of core (max 64) for your parallel job
-#SBATCH --cpus-per-task=3                               # Number of CPUs per task
+#SBATCH --cpus-per-task=6                               # number of cores per task (e.g. OpenMP)
+#SBATCH --mem-per-cpu=1G
 
 # Put your email here to receive execution location by mail
 echo "" |mail -s "Your job $SLURM_JOB_ID is running on $HOSTNAME" lui.habl@lpp.polytechnique.fr
 
 # Here you have to charge your module
 
+module purge
+module load slurm/14.03.0
+module load gcc/6.3.0
+module load make/4.2
 module load openmpi/gcc/64/3.1.4
+
+export PATH=$HOME/opt/bin:$PATH
 
 # Here the code to execute with the SRUN command
 
-mpirun --mca btl self,openib,vader -np $SLURM_NTASKS ./run
+#mpirun --mca btl self,openib,vader -np $SLURM_NTASKS ./run -n 1 &
+#mpirun --mca btl self,openib,vader -np $SLURM_NTASKS ./run -n 2 &
+#mpirun --bind-to none --mca btl openib,self,vader -np 1 ./run
 
+#parallel --max-procs=${SLURM_CPUS_PER_TASK} "$cmd,subjob={1}; sleep 30" ::: {1..5}
+
+#for sj in {0..2}
+#do
+#    mpirun --bind-to none --mca btl openib,self,vader -np 1 ./run -n $sj > output/log/${SLURM_JOB_ID}_${sj}.out &
+#done
+#wait
+
+mpirun --bind-to none --mca btl openib,self,vader -np 1 ./run -n 0 > output/log/${SLURM_JOB_ID}_0.out &
+mpirun --bind-to none --mca btl openib,self,vader -np 1 ./run -n 1 > output/log/${SLURM_JOB_ID}_1.out &
+wait
+
+#mpi_cmd="mpirun --bind-to none --mca btl openib,self,vader -np 1"
+#parallel_cmd="parallel --delay .2 -j $SLURM_NTASKS"
+#$parallel_cmd "$mpi_cmd ./run{1} -n > output/log/${SLURM_JOB_ID}_{1}.out" ::: {0..2}
+
+# mpirun --bind-to none --mca btl openib,self,vader -np $SLURM_NTASKS ./run
 # Make sure you return zero as exit code otherwiser SLURM will report your job
 # as failed.
 exit 0
