c4b4ded09b6605c4065932fcb2af9b8b651ace1d
Lui Txai Calvoso Habl <txaicalvoso@gmail.com>
2020-10-20 13:38:42 +0200

    Generalizing make, gitignore and updating files

--- util/load_output.py
+++ util/load_output.py
@@ -27,28 +27,36 @@ class OutputFile:
     def __getitem__(self, path):
         return reduce(operator.getitem, path.split(self.sep), self.file)
 
-    def field_img(self, path, label=''):
+    def field_img(self, path, label='', inner_label='', save=False, name='field.pdf', multiplier=1, add_factor=0, **kwargs):
 
         field = np.transpose(self[path])
 
         fig, axes = plt.subplots(nrows=1, figsize=(10, 4))
 
-        p = axes.imshow(field, cmap='plasma', origin='lower')
-
-        axes.xaxis.set_major_locator(plt.MultipleLocator(64))
-        axes.yaxis.set_major_locator(plt.MultipleLocator(16))
-
-        divider0 = make_axes_locatable(axes)
-        cax0 = divider0.append_axes("right", size="1%", pad=0.1)
+        p = axes.imshow(field * multiplier + add_factor, interpolation='nearest', origin='lower', **kwargs)
         
-        fig.colorbar(p, ax=axes,cax=cax0, label=label)
+#         axes.xaxis.set_major_locator(plt.MultipleLocator(0.5))
+#         axes.yaxis.set_major_locator(plt.MultipleLocator(0.5))
+#         axes.xaxis.set_minor_locator(plt.MultipleLocator(0.1))
+#         axes.yaxis.set_minor_locator(plt.MultipleLocator(0.25))
+
+#         divider0 = make_axes_locatable(axes)
+#         cax0 = divider0.append_axes("right", size="1%", pad=0.1)
+#         fig.colorbar(p, ax=axes,cax=cax0, label=label)
+        fig.colorbar(p, fraction=0.01225, pad=0.01, label=label)
+    
+#         axes.axis('image')
+        axes.text(0.98, 0.78, inner_label, horizontalalignment='right', verticalalignment='bottom', transform=axes.transAxes, color='white')
+        axes.set_ylabel('$y / L_y$')
+        axes.set_xlabel('$x / L_x$')
         
-        axes.axis('image')
-        axes.set_ylabel('$N_y$')
-        axes.set_xlabel('$N_x$')
+        if save:
+            fig.savefig(name,  bbox_inches='tight')
 
         plt.tight_layout()
         plt.show()
+        
+        return fig, axes
 
     def scatter(self, path, cols=(0,1), setlim=True, color='white', background='dark'):
         x = self[path][:,cols[0]]
@@ -68,13 +76,15 @@ class OutputFile:
         plt.tight_layout()
         plt.show()
     
-    def field_contour(self, path, num=50, label=''):
+    def field_contour(self, path, num=50, label='', **kwargs):
         field = np.transpose(self[path])
-        x = np.transpose(self['mesh.x'])
-        y = np.transpose(self['mesh.y'])
+#         x = np.transpose(self['mesh.x'])
+#         y = np.transpose(self['mesh.y'])
+        x = np.arange(512)
+        y = np.arange(128)
         
         fig, axes = plt.subplots(nrows=1, figsize=(10, 4))
-        p = axes.contourf(x,y, field, num, cmap='plasma')
+        p = axes.contourf(x,y, field, num, **kwargs)
 
         divider0 = make_axes_locatable(axes)
         cax0 = divider0.append_axes("right", size="1%", pad=0.1)
@@ -82,9 +92,8 @@ class OutputFile:
         fig.colorbar(p, ax=axes,cax=cax0, label=label)
 
         axes.axis('image')
-        axes.set_ylabel('$y (m)$')
-        axes.set_xlabel('$x (m)$')
-
+        axes.set_ylabel('$y / \lambda_D$')
+        axes.set_xlabel('$x / \lambda_D$')
         plt.show()
 
     def plot_line(self, x1_path, x2_path=None):
@@ -93,6 +102,31 @@ class OutputFile:
         else:
             plt.plot(self[x1_path].value, self[x2_path].value, 'r')
         plt.show()
+    
+    def plot_series(self, y_path, xlabel=None, ylabel=None, xmag=None, title=None, **kwargs):
+        path_comp = y_path.split(self.sep)
+        t = self[self.sep.join(path_comp[:-1])]['time'].value
+        y = self[y_path].value
+        
+        if xlabel is None:
+            plt.xlabel('Time [s]')
+        else:
+            plt.xlabel(xlabel)
+        
+        if title is not None:
+            plt.title(title)
+        
+        if ylabel is not None:
+            plt.ylabel(ylabel)
+        
+        tdiv = 1
+        if xmag is not None:
+            tdiv = xmag
+        
+        plt.plot(t / tdiv, y, **kwargs)
+        plt.tight_layout()
+        
+        
     
     def dist(self, path, col=0, bins='auto'):
         plt.hist(self[path][:,col], bins=bins, histtype='step', color='r')
@@ -103,6 +137,36 @@ class OutputFile:
         for i, c in enumerate(cols):
             t[i] = np.mean(0.5 * self[path][:,c]**2 * mass) / e
         return t
+        
+    def plot_vdist(self, vdist_path, title=None, **kwargs):
+        path_comp = vdist_path.split(self.sep)
+        vlim = self[self.sep.join(path_comp[:-1])].attrs['vlim_' + path_comp[-1]]
+        n_v = self[self.sep.join(path_comp[:-1])].attrs['n_v']
+        v = np.linspace(vlim[0], vlim[1], n_v)
+        
+        vdist = self[vdist_path].value
+        
+        plt.ylabel('Norm. distribution')
+        plt.xlabel('v [m/s]')
+        
+        if title is not None:
+            plt.title(title)
+        
+        plt.plot(v, vdist / vdist.max(), **kwargs)
+        
+        plt.legend()
+        plt.tight_layout()
+        
+        
+def gather_data(d, key):
+    v_list = []
+    for k in sorted(d):
+        v = d[k][key][()].copy()
+        v_list.append(v)
+    return np.concatenate(v_list)
+
+def n(a, n_avg):
+    return np.mean(a.reshape(-1, n_avg), axis=1)
 
 
 def field_img(field, label=''):
@@ -161,6 +225,25 @@ def load_all(folder_path='.'):
     return d
 
 
+def load_latest(folder_path='.'):
+    df = None
+    folder = Path(folder_path)
+    file_list = sorted(folder.glob('*'))
+    f = file_list[-1]
+    if f.suffix == '.exdir':
+        print(file_list[-1])
+        return OutputFile(load_file_data_exdir(file_list[-1]))
+    else:
+        print(f'Incompatible format: {f}')
+
+
+def load_list(base_path, name_list):
+    base_folder = Path(base_path)    
+    d = []
+    for n in name_list:
+        d.append(load_latest(base_folder / n))
+    return d
+        
 if __name__=='__main__':
 
     parser = argparse.ArgumentParser(description='Loads all the exdir or hdf5 files in a dir and provides plotting functions.')
