437ee00ea970b72182176165a7f81d048d126abe
Lui Habl <txaicalvoso@gmail.com>
2021-06-25 12:45:48 +0200

    Removing unused headers

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,11 @@
-
-#include <chrono>
-#include <cmath>
-#include <iostream>
-#include <iomanip>
-#include <sstream>
 #include <string>
-#include <unordered_map>
 
 #include "mpi.h"
 #include "arg-parser.h"
 #include "simulation.h"
-
-#include "fields.h"
-#include "fmatrix.h"
-#include "fmath.h"
-#include "mcc.h"
-
-#include "particles.h"
-#include "particles-in-mesh.h"
-#include "rsolver.h"
-#include "input-output.h"
-#include "num-tools.h"
-#include "dsmc.h"
-#include "state-info.h"
-
 #include "configuration.h"
-#include "diagnostics.h"
-#include "clock.h"
 
 #define DEFAULT_CONFIG_PATH "input/config/config.yaml"
-
-using namespace std;
-using namespace std::chrono;
 
 // ----------------------------- Main function --------------------------------
 int main(int argc, char* argv[])
@@ -46,7 +20,7 @@ int main(int argc, char* argv[])
     // ------------------- Loading configuration ------------------------------
 
     argparser arg(argc, argv);
-    string config_path = arg.get("config", DEFAULT_CONFIG_PATH);
+    std::string config_path = arg.get("config", DEFAULT_CONFIG_PATH);
     configuration config(arg.get("config", DEFAULT_CONFIG_PATH));
     config.set_job_name(arg.get("name", ""));
 
